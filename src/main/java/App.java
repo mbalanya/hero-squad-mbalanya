7f8075822db2a04987ea6ad6baf41a71b6c6edaa
mbalanya <okobacharles@yahoo.co.uk>
2020-07-08 13:28:55 +0300

    update app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,7 +13,17 @@ import static spark.Spark.post;
 import static spark.Spark.staticFileLocation;
 
 public class App {
+    private static void port(int herokuAssignedPort) {
+    }
+    private static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
     public static void main(String[] args){
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
 
         get("/", (request, response) -> {
@@ -56,4 +66,6 @@ public class App {
             return new ModelAndView(model, "heros.hbs");
         }, new HandlebarsTemplateEngine());
     }
+
+
 }
